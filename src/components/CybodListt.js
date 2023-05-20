827644bd37b96bec168f12f7ae2fc6025a93a0c7
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-05-20 16:51:15 +0300

    passing state in props

--- src/components/CybodListt.js
+++ src/components/CybodListt.js
@@ -1,30 +1,43 @@
 import { useEffect, useState } from "react";
 import CategoryFilter from "./CategoryFilter";
-import CybugCard from "./CybugCard";
+import CyborgCard from "./CyborgCard";
 
-function CarList(){
+function CarList() {
     const [selectedCategory, setSelectedCategory] = useState('ALL');
     const [cyborgs, setCyborgs] = useState([]);
 
 
     useEffect(() => {
         fetch('/cyborgs')
-          .then((res) => res.json())
-          .then((dataArrays) => {
-            setCyborgs(dataArrays);
-          });
-      }, []);
+            .then((res) => res.json())
+            .then((dataArrays) => {
+                setCyborgs(dataArrays);
+            });
+    }, []);
 
-      const categories = cyborgs
-      .map((cyborg) => cyborg.category)
-      .filter(
-        (category, index, categories) => categories.indexOf(category) === index)
-      .sort();
+    const categories = cyborgs
+        .map((cyborg) => cyborg.category)
+        .filter(
+            (category, index, categories) => categories.indexOf(category) === index)
+        .sort();
 
-      const displayedCyborg = cyborgs.filter(
+    const displayedCyborg = cyborgs.filter(
         (cyborg) => selectedCategory === "All" || cyborg.category === selectedCategory
-      );
-    return(
-        <div ></div>
+    );
+    return (
+        <div >
+            <Sidebar>
+                <CategoryFilter
+                    categories={["All", ...categories]}
+                    selectedCategory={selectedCategory}
+                    setSelectedCategory={setSelectedCategory}
+                />
+            </Sidebar>
+            <section>
+                {displayedCyborg.map((cyborg) => (
+                    <CyborgCard key={cyborg.id} movie={cyborg} />
+                ))}
+            </section>
+        </div>
     )
 }
