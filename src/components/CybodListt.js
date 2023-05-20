549e1ec38fe62fce0712a41590cef54e6afad226
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-05-20 16:45:27 +0300

    new code

--- src/components/CybodListt.js
+++ src/components/CybodListt.js
@@ -4,8 +4,26 @@ import CybugCard from "./CybugCard";
 
 function CarList(){
     const [selectedCategory, setSelectedCategory] = useState('ALL');
-    const [cybug, setCybug] = useState([]);
-    
+    const [cyborgs, setCyborgs] = useState([]);
+
+
+    useEffect(() => {
+        fetch('/cyborgs')
+          .then((res) => res.json())
+          .then((dataArrays) => {
+            setCyborgs(dataArrays);
+          });
+      }, []);
+
+      const categories = cyborgs
+      .map((cyborg) => cyborg.category)
+      .filter(
+        (category, index, categories) => categories.indexOf(category) === index)
+      .sort();
+
+      const displayedCyborg = cyborgs.filter(
+        (cyborg) => selectedCategory === "All" || cyborg.category === selectedCategory
+      );
     return(
         <div ></div>
     )
