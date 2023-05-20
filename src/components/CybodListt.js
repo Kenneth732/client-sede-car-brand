864b6c69cc3ab8021a55348a0df66c9dbf9777a8
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-05-20 16:39:16 +0300

    new compo for rendering

---
+++ src/components/CybodListt.js
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+import CategoryFilter from "./CategoryFilter";
+import CybugCard from "./CybugCard";
+
+function CarList(){
+    const [selectedCategory, setSelectedCategory] = useState('ALL');
+    const [cybug, setCybug] = useState([]);
+    
+    return(
+        <div ></div>
+    )
+}
